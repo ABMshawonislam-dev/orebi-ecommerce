74f88d8114bc99bea93e25dcec46349b73c8c7d2
A B M Shawon Islam <abmshawonislam@gmail.com>
2022-12-08 12:45:18 +0600

    user dorpdown header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,22 +2,31 @@ import Container from "./Container";
 import React, { useEffect, useRef, useState } from "react";
 import Flex from "./Flex";
 import Dropdown from "./Dropdown";
-import { FaBars, FaSearch } from "react-icons/fa";
+import { FaBars, FaSearch, FaUserAlt } from "react-icons/fa";
+import { RxTriangleDown } from "react-icons/rx";
+import { HiShoppingCart } from "react-icons/hi";
 import List from "./List";
 import ListItem from "./ListItem";
 import Search from "./Search";
 
 const Header = () => {
-  let [show, setShow] = useState(false);
-  let ref = useRef();
+  let [categoryDropDownShow, setCategoryDropDownShow] = useState(false);
+  let [userDropSownShow, setUserDropSownShow] = useState(false);
+  let categooryRef = useRef();
+  let userRef = useRef();
 
   useEffect(() => {
     document.body.addEventListener("click", (e) => {
-      console.log(ref.current);
-      if (ref.current.contains(e.target)) {
-        setShow(true);
+      if (categooryRef.current.contains(e.target)) {
+        setCategoryDropDownShow(true);
       } else {
-        setShow(false);
+        setCategoryDropDownShow(false);
+      }
+
+      if (userRef.current.contains(e.target)) {
+        setUserDropSownShow(true);
+      } else {
+        setUserDropSownShow(false);
       }
     });
   }, []);
@@ -26,34 +35,34 @@ const Header = () => {
       <Container>
         <Flex className="flex justify-between">
           <div className="flex items-center">
-            <Dropdown className="relative" dropref={ref}>
+            <Dropdown className="relative" dropref={categooryRef}>
               <p className="font-regular flex items-center gap-x-2.5 font-dm text-sm">
                 <FaBars /> Shop by Category
               </p>
-              {show && (
+              {categoryDropDownShow && (
                 <List className="font-regular absolute top-8 w-[263px] bg-primary font-dm text-sm text-[#767676]">
                   <ListItem
-                    className="border border-solid border-[#2D2D2D] py-4 px-5 duration-100 ease-in hover:px-7 hover:font-bold hover:text-white"
+                    className="border-b border-solid border-[#2D2D2D] py-4 px-5 duration-100 ease-in hover:px-7 hover:font-bold hover:text-white"
                     itemname="Accesories"
                   />
                   <ListItem
-                    className="border-solid border-[#2D2D2D] py-4 px-5 duration-100 ease-in hover:px-7 hover:font-bold hover:text-white"
+                    className="border-b border-solid border-[#2D2D2D] py-4 px-5 duration-100 ease-in hover:px-7 hover:font-bold hover:text-white"
                     itemname="Furniture "
                   />
                   <ListItem
-                    className="border-solid border-[#2D2D2D] py-4 px-5 duration-100 ease-in hover:px-7 hover:font-bold hover:text-white"
+                    className="border-b border-solid border-[#2D2D2D] py-4 px-5 duration-100 ease-in hover:px-7 hover:font-bold hover:text-white"
                     itemname="Electronics"
                   />
                   <ListItem
-                    className="v border-solid border-[#2D2D2D] py-4 px-5 duration-100 ease-in hover:px-7 hover:font-bold hover:text-white"
+                    className="border-b border-solid border-[#2D2D2D] py-4 px-5 duration-100 ease-in hover:px-7 hover:font-bold hover:text-white"
                     itemname="Clothes"
                   />
                   <ListItem
-                    className="border-solid border-[#2D2D2D] py-4 px-5 duration-100 ease-in hover:px-7 hover:font-bold hover:text-white"
+                    className="border-b border-solid border-[#2D2D2D] py-4 px-5 duration-100 ease-in hover:px-7 hover:font-bold hover:text-white"
                     itemname="Bags"
                   />
                   <ListItem
-                    className="border-solid border-[#2D2D2D] py-4 px-5 duration-100 ease-in hover:px-7 hover:font-bold hover:text-white"
+                    className="border-b border-solid border-[#2D2D2D] py-4 px-5 duration-100 ease-in hover:px-7 hover:font-bold hover:text-white"
                     itemname="Home appliances"
                   />
                 </List>
@@ -67,7 +76,33 @@ const Header = () => {
             />
             <FaSearch className="absolute top-4 right-4" />
           </div>
-          <div className="flex items-center">ad</div>
+          <div className="flex items-center">
+            <Flex className="flex gap-x-10">
+              <Dropdown className="relative" dropref={userRef}>
+                <div className="flex items-center ">
+                  <FaUserAlt />
+                  <RxTriangleDown />
+                </div>
+
+                {userDropSownShow && (
+                  <List className="font-regular absolute top-8 right-0 w-[200px] border border-solid border-[#F0F0F0] bg-white text-center font-dm text-sm text-primary">
+                    <ListItem
+                      className="border-b border-solid border-[#F0F0F0] py-4 px-5 duration-100 ease-in hover:bg-primary hover:bg-primary hover:font-bold hover:text-white"
+                      itemname="My Account"
+                    />
+                    <ListItem
+                      className="border-b border-solid border-[#F0F0F0] py-4 px-5 duration-100 ease-in hover:bg-primary hover:font-bold hover:text-white"
+                      itemname="Log Out "
+                    />
+                  </List>
+                )}
+              </Dropdown>
+
+              <div>
+                <HiShoppingCart />
+              </div>
+            </Flex>
+          </div>
         </Flex>
       </Container>
     </div>
