adef2ae7f3e364f96edf1ab72ab3120b5d061b02
A B M Shawon Islam <abmshawonislam@gmail.com>
2022-12-08 17:08:05 +0600

    header part responsive

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,18 +2,23 @@ import Container from "./Container";
 import React, { useEffect, useRef, useState } from "react";
 import Flex from "./Flex";
 import Dropdown from "./Dropdown";
+import Image from "./Image";
 import { FaBars, FaSearch, FaUserAlt } from "react-icons/fa";
 import { RxTriangleDown } from "react-icons/rx";
 import { HiShoppingCart } from "react-icons/hi";
+import { ImCross } from "react-icons/im";
 import List from "./List";
 import ListItem from "./ListItem";
 import Search from "./Search";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   let [categoryDropDownShow, setCategoryDropDownShow] = useState(false);
   let [userDropSownShow, setUserDropSownShow] = useState(false);
+  let [cartDropSownShow, setCartDropSownShow] = useState(false);
   let categooryRef = useRef();
   let userRef = useRef();
+  let cartRef = useRef();
 
   useEffect(() => {
     document.body.addEventListener("click", (e) => {
@@ -28,6 +33,12 @@ const Header = () => {
       } else {
         setUserDropSownShow(false);
       }
+
+      if (cartRef.current.contains(e.target)) {
+        setCartDropSownShow(true);
+      } else {
+        setCartDropSownShow(false);
+      }
     });
   }, []);
   return (
@@ -37,7 +48,8 @@ const Header = () => {
           <div className="flex items-center">
             <Dropdown className="relative" dropref={categooryRef}>
               <p className="font-regular flex items-center gap-x-2.5 font-dm text-sm">
-                <FaBars /> Shop by Category
+                <FaBars />{" "}
+                <span className="hidden lg:inline-block">Shop by Category</span>
               </p>
               {categoryDropDownShow && (
                 <List className="font-regular absolute top-8 w-[263px] bg-primary font-dm text-sm text-[#767676]">
@@ -69,9 +81,9 @@ const Header = () => {
               )}
             </Dropdown>
           </div>
-          <div className="relative w-[600px]">
+          <div className="relative w-auto lg:w-[600px]">
             <Search
-              className="font-regular w-full py-4 px-5 font-dm text-sm placeholder:text-[#c4c4c4]"
+              className="font-regular w-full py-4 px-5 font-dm text-sm placeholder:text-[#c4c4c4] "
               placeholder="Search Products"
             />
             <FaSearch className="absolute top-4 right-4" />
@@ -99,7 +111,50 @@ const Header = () => {
               </Dropdown>
 
               <div>
-                <HiShoppingCart />
+                <Dropdown className="relative" dropref={cartRef}>
+                  <HiShoppingCart className="text-2xl" />
+
+                  {cartDropSownShow && (
+                    <div className="absolute top-8 right-0 w-[360px] border border-solid border-[#F0F0F0]">
+                      <div className=" bg-[#F5F5F3] p-5">
+                        <Flex className="flex justify-between">
+                          <div>
+                            <Image imgsrc="assets/cartimage.png" />
+                          </div>
+                          <div className="flex flex-col justify-center">
+                            <h3 className="font-dm text-sm font-bold text-primary">
+                              Black Smart Watch
+                            </h3>
+                            <p className="mt-3 font-dm text-sm font-bold text-primary">
+                              $44
+                            </p>
+                          </div>
+                          <div className="flex items-center justify-end">
+                            <ImCross className="text-sm" />
+                          </div>
+                        </Flex>
+                      </div>
+                      <div className="bg-white p-5">
+                        <h4 className="font-regular font-dm text-base text-[#767676]">
+                          Subtotal:{" "}
+                          <span className="font-bold text-primary">$44.00</span>
+                        </h4>
+                        <Link
+                          to="#"
+                          className="mt-3 inline-block border border-solid border-primary  py-4 px-10 font-dm text-sm font-bold"
+                        >
+                          View Cart
+                        </Link>
+                        <Link
+                          to="#"
+                          className="mt-3 ml-5 inline-block border border-solid border-primary bg-primary py-4 px-10 font-dm text-sm font-bold text-white"
+                        >
+                          Checkout
+                        </Link>
+                      </div>
+                    </div>
+                  )}
+                </Dropdown>
               </div>
             </Flex>
           </div>
