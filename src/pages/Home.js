1d300914cbff76b6840495d96f569c72277d58d8
A B M Shawon Islam <111572423+merncit@users.noreply.github.com>
2022-12-14 18:13:05 +0600

    product

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
 import AdditionalInfo from "../components/layout/AdditionalInfo";
+import Advertise from "../components/layout/Advertise";
 import Banner from "../components/layout/Banner";
+import Newarrival from "../components/layout/Newarrival";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <AdditionalInfo />
+      <Advertise />
+      <Newarrival />
     </>
   );
 };
